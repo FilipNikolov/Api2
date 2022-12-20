00d775f1dad815734a1872ab0ad199df6493934d
FilipNikolov <nikolovfilip98@gmail.com>
2022-12-20 10:33:08 +0100

    init

--- web/src/index.js
+++ web/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './css/index.css';
 import { App } from './App';
+import { Homepage } from './components/Homepage';
+import { GetStarted } from './components/GetStarted';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
+      <Route path="" element={<App />} />
+      <Route path="/homepage" element={<Homepage />} />
+      <Route path="/getstarted" element={<GetStarted />} />
       <Route path="" element={<App />} />
     </Routes>
   </Router>
